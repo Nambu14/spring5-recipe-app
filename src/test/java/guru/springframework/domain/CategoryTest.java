a64b95ab3e5d185f4edd4590a98fe4350ede19fb
A307812 <lalvarezhamann@santandertecnologia.com.ar>
2020-08-24 16:54:06 -0300

    moquito

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.domain;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+ }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
