756eb20706ddb8eb399a38af1c1a93faa44f5be4
A307812 <lalvarezhamann@santandertecnologia.com.ar>
2020-08-26 12:21:01 -0300

    delete recipe

--- src/main/java/guru/springframework/services/RecipeService.java
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -6,11 +6,14 @@ import guru.springframework.domain.Recipe;
 import java.util.Set;
 
 public interface RecipeService {
+
     Set<Recipe> getRecipes();
 
     Recipe findById(Long l);
 
+    RecipeCommand findCommandById(Long l);
+
     RecipeCommand saveRecipeCommand(RecipeCommand command);
 
-    RecipeCommand findCommandById(Long id);
+    void deleteById(Long idToDelete);
 }
